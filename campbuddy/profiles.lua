9ba9d628e8c186ca2b0a5cc9d6663be1f5214b68
Aessk <aesk.99@gmail.com>
2023-07-30 22:22:31 -0400

    Add files via upload

--- campbuddy/profiles.lua
+++ campbuddy/profiles.lua
@@ -1,47 +1,65 @@
 local profiles = {}
 
+--[[
+		[''] = {
+			nickname = '',
+			group = '',
+			zone = '',
+			placeholders = {
+				[''] = 000,
+			};
+		},
+]]--
+
 local profiles = T{
 	PH = {
-		['Charybdis'] = {
-			nickname = 'charby',
-			group = 'ssg',
-			zone = 'SEA SERPENT GROTTO',
+	-- ATTOHWA CHASM
+		['Ambusher Antlion'] = {
+			nickname = 'ambusher',
+			group = 'chasm',
+			zone = 'ATTOHWA CHASM',
 			placeholders = {
-				['196'] = 976,
-				['198'] = 976,
-				['199'] = 976
+				['11B'] = 976, --respawn is?
 			};
 		},
-		['Hoo Mjuu The Torrent'] = {
-			nickname = 'mjuu',
-			group = 'giddeus',
-			zone = 'GIDDEUS',
-			['179'] = 976
-		},
-		['Jaggedy-Eared Jack'] = {
-			nickname = 'jej',
-			group = 'ronf',
-			zone = 'WEST RONFAURE',
+		['Citipati'] = {
+			nickname = 'citi',
+			group = 'chasm',
+			zone = 'ATTOHWA CHASM',
 			placeholders = {
-				['126'] = 346
+				['10B'] = 976, --respawn is?
+				['10E'] = 976, --respawn is?
+				['111'] = 976, --respawn is?
 			};
 		},
-		['Leaping Lizzy'] = {
-			nickname = 'll',
-			group = 'gusta',
-			zone = 'SOUTH GUSTABERG',
+	-- BIBIKI BAY
+		['Serra'] = {
+			nickname = 'serra',
+			group = 'bibiki',
+			zone = 'BIBIKI BAY',
 			placeholders = {
-				['17B'] = 346
+				['02D'] = 346,
 			};
 		},
-		['Lord of Onzozo'] = {
-			nickname = 'loo',
-			group = 'onzozo',
-			zone = 'LABYRINTH OF ONZOZO',
+		['Intulo'] = {
+			nickname = 'intulo',
+			group = 'bibiki',
+			zone = 'BIBIKI BAY',
 			placeholders = {
-				['042'] = 976
+				['08D'] = 346,
 			};
 		},
+	-- BOSTAUNIEUX OUBLIETTE
+		['Sewer Syrup'] = {
+			nickname = 'syrup',
+			group = 'sewers',
+			zone = 'BOSTAUNIEUX OUBLIETTE',
+			placeholders = {
+				['039'] = 976,
+				['03A'] = 976,
+			};
+		},
+	-- CASTLE OZTROJA
 		['Mee Deggi the Punisher'] = {
 			nickname = 'deggi',
 			group = 'castleo',
@@ -60,6 +78,120 @@ local profiles = T{
 				['09C'] = 976
 			};
 		},
+	-- GIDDEUS
+		['Hoo Mjuu The Torrent'] = {
+			nickname = 'mjuu',
+			group = 'giddeus',
+			zone = 'GIDDEUS',
+			['179'] = 976
+		},
+	-- KUFTAL TUNNEL
+		['Devil Manta'] = {
+			nickname = 'manta',
+			group = 'angelskin',
+			zone = 'KUFTAL TUNNEL',
+			placeholders = {
+				['005'] = 616,
+			};
+		},
+	-- LABYRINTH OF ONZOZO
+		['Lord of Onzozo'] = {
+			nickname = 'loo',
+			group = 'onzozo',
+			zone = 'LABYRINTH OF ONZOZO',
+			placeholders = {
+				['042'] = 976
+			};
+		},
+		['Ose'] = { 
+			nickname = 'Ose',
+			group = 'onzozo',
+			zone = 'LABYRINTH OF ONZOZO',
+			placeholders = { 
+				['095'] = 976,
+				['096'] = 976,
+				['0A0'] = 976,
+				['09C'] = 976,
+				['09F'] = 976,
+				['098'] = 976,
+				['097'] = 976,
+			};
+		},
+		['Soulstealer Skullnix'] = { 
+			nickname = 'Soulstealer',
+			group = 'onzozo',
+			zone = 'LABYRINTH OF ONZOZO',
+			placeholders = { 
+				['0B5'] = 976,
+				['0A1'] = 976,
+			};
+		},
+	-- LUFAISE MEADOWS
+		['Megalobugard'] = {
+			nickname = 'megalo',
+			group = 'lufaise',
+			zone = 'LUFAISE MEADOWS',
+			placeholders = {
+				['0C8'] = 346,
+			};
+		},
+	-- QUICKSAND CAVES
+		['Centurio X-I'] = {
+			nickname = 'centxi',
+			group = 'qsc',
+			zone = 'QUICKSAND CAVES',
+			placeholders = {
+				['035'] = 436,
+			};
+		},
+	-- SEA SERPENT GROTTO
+		['Charybdis'] = {
+			nickname = 'charby',
+			group = 'ssg',
+			zone = 'SEA SERPENT GROTTO',
+			placeholders = {
+				['196'] = 976,
+				['198'] = 976,
+				['199'] = 976
+			};
+		},
+	-- SOUTH GUSTABERG
+		['Leaping Lizzy'] = {
+			nickname = 'll',
+			group = 'gusta',
+			zone = 'SOUTH GUSTABERG',
+			placeholders = {
+				['17B'] = 346
+			};
+		},
+	-- THE BOYAHDA TREE
+		['Aquarius'] = { 
+			nickname = 'Aquar',
+			group = 'tree',
+			zone = 'THE BOYAHDA TREE',
+			placeholders = { 
+				['059'] = 976,
+				['05A'] = 976,
+				['05B'] = 976,
+				['05D'] = 976,
+				['05E'] = 976,
+				['05F'] = 976,
+				['062'] = 976,
+				['063'] = 976,
+				['064'] = 976,
+			};
+		},
+	-- THE GARDEN OF RUHMET
+		['Ix\'aern'] = {
+			nickname = 'ix',
+			group = 'sea',
+			zone = 'THE GARDEN OF RUHMET',
+			placeholders = {
+				['1BA'] = 60,
+				['1BE'] = 60,
+			}
+		},
+	-- VALKURM DUNES
 		['Valkurm Emepror'] = {
 			nickname = 've',
 			group = 'dunes',
@@ -76,21 +208,13 @@ local profiles = T{
 				['1CA'] = 346
 			};
 		},
-		['Ix\'aern'] = {
-			nickname = 'ix',
-			group = 'sea',
-			zone = 'THE GARDEN OF RUHMET',
+	-- WEST RONFAURE
+		['Jaggedy-Eared Jack'] = {
+			nickname = 'jej',
+			group = 'ronf',
+			zone = 'WEST RONFAURE',
 			placeholders = {
-				['0BA'] = 60,
-				['0BE'] = 60,
-			}
-		},
-		['Devil Manta'] = {
-			nickname = 'manta',
-			group = 'angelskin',
-			zone = 'KUFTAL TUNNEL',
-			placeholders = {
-				['005'] = 616,
+				['126'] = 346
 			};
 		},
 	},
